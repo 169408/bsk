bb4ec9ba4f87f5f5a03f31ba9653e202a069516a
Ruslan <115550042+169408@users.noreply.github.com>
2024-11-22 16:45:22 +0100

    Update generate.php

--- aes_kurrr/generate.php
+++ aes_kurrr/generate.php
@@ -1,23 +1,23 @@
 <?php
 
-$message = "Hello, this is a secret message! Don't tell anyone that you got it, You must help Me. Right now I'm sitting in the prison"; // Повідомлення для шифрування
+$message = "Hello, this is a secret message! Don't tell anyone that you got it, You must help Me. Right now I'm sitting in the prison"; // Wiadomość do zaszyfrowania
 
-// Генерація випадкового 128-бітного ключа (16 байтів)
+// Generowanie losowego 128-bitowego klucza (16 bajtów)
 $key = openssl_random_pseudo_bytes(16);
 echo "Generated Key (hex): " . bin2hex($key) . "\n";
 
-// Збереження ключа у файл
+// Zapisanie klucza do pliku
 file_put_contents('aes_key.bin', $key);
 
-// Ініціалізація шифру AES у режимі ECB
+// Inicjalizacja szyfrowania AES w trybie ECB
 $encryptedMessage = openssl_encrypt($message, 'AES-128-ECB', $key, OPENSSL_RAW_DATA);
 
-// Перевірка успішності шифрування
+// Sprawdzenie poprawności szyfrowania
 if ($encryptedMessage === false) {
     echo "Encryption failed.\n";
     exit;
 }
 
-// Збереження зашифрованого повідомлення у файл
+// Zapisanie zaszyfrowanej wiadomości do pliku
 file_put_contents('aes_encrypted_message.bin', $encryptedMessage);
 echo "Encrypted message saved successfully.\n";
